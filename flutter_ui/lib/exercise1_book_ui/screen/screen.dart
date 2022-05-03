a7a6eee5294d9159594143173c98c1cc0b659efd
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-03 22:54:55 +0700

    update screen and main

--- flutter_ui/lib/exercise1_book_ui/screen/screen.dart
+++ flutter_ui/lib/exercise1_book_ui/screen/screen.dart
@@ -1,13 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/exercise1_book_ui/screen/screen_detail.dart';
+import 'package:flutter_ui/exercise1_book_ui/widgets/widgets.dart';
 
-class Screen extends StatelessWidget {
-  const Screen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Movie App"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "News Book",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontSize: 25),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CustomCard(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const DetailScreen()));
+                      },
+                      price: "\$20",
+                      title: "Cate",
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPDT3gJddVjkoCQkI4qs3tt-rUju8QEVrWBlTQ0N6RjgJh_58-nLrRkjWtGCIu5JT12Xw&usqp=CAU",
+                    ),
+                    const CustomCard(
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPDT3gJddVjkoCQkI4qs3tt-rUju8QEVrWBlTQ0N6RjgJh_58-nLrRkjWtGCIu5JT12Xw&usqp=CAU",
+                    ),
+                    const CustomCard(
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPDT3gJddVjkoCQkI4qs3tt-rUju8QEVrWBlTQ0N6RjgJh_58-nLrRkjWtGCIu5JT12Xw&usqp=CAU",
+                    ),
+                    const CustomCard(
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPDT3gJddVjkoCQkI4qs3tt-rUju8QEVrWBlTQ0N6RjgJh_58-nLrRkjWtGCIu5JT12Xw&usqp=CAU",
+                    ),
+                    const CustomCard(
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPDT3gJddVjkoCQkI4qs3tt-rUju8QEVrWBlTQ0N6RjgJh_58-nLrRkjWtGCIu5JT12Xw&usqp=CAU",
+                    ),
+                    const CustomCard(
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPDT3gJddVjkoCQkI4qs3tt-rUju8QEVrWBlTQ0N6RjgJh_58-nLrRkjWtGCIu5JT12Xw&usqp=CAU",
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "For You",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontSize: 25),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: const [
+                    CustomCard(
+                      price: "\$20",
+                      title: "BTC",
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                    CustomCard(
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                    CustomCard(
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                    CustomCard(
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                    CustomCard(
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                    CustomCard(
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                    CustomCard(
+                      imageUrl:
+                          "https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg",
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/exercise1_book_ui/screen/screen.dart';
-import 'package:flutter_ui/introduction_to_widgets/widget1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,27 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Column(
-          children: [
-            MyApp1(
-              title: Text(
-                "Hello",
-                style: Theme.of(context).primaryTextTheme.headline6,
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text(
-                  "Hello World!",
-                  style: Theme.of(context).textTheme.displayLarge,
-                ),
-              ),
-            ),
-          ],
-        ),
+      home: Scaffold(
+        body: HomeScreen(),
       ),
     );
   }
