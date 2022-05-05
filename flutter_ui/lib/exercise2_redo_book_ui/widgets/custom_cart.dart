36df721de3a4a11f7d5fcfe7ae710011623e77ad
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-05 23:59:32 +0700

    Update custom_cart.dart

--- flutter_ui/lib/exercise2_redo_book_ui/widgets/custom_cart.dart
+++ flutter_ui/lib/exercise2_redo_book_ui/widgets/custom_cart.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 
 class Custom_Card extends StatelessWidget {
   final String? imageURL;
-  const Custom_Card({Key? key, this.imageURL}) : super(key: key);
+  final String? title;
+  final String? price;
+  const Custom_Card({Key? key, this.imageURL, this.price, this.title})
+      : super(key: key);
+
+  Widget priceLable(String price) {
+    return Container();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(right: 1),
       height: MediaQuery.of(context).size.height * 0.2,
       width: MediaQuery.of(context).size.width * 0.4,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         image: DecorationImage(
+          fit: BoxFit.cover,
           image: NetworkImage(imageURL ?? ""),
         ),
       ),
+    );
+    const SizedBox(
+      height: 5,
     );
   }
 }
