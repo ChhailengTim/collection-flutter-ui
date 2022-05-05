663f9c43d436eeb5015f372f57e5321be2f334e2
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-05 23:44:35 +0700

    Create home_screen.dart

---
+++ flutter_ui/lib/exercise2_redo_book_ui/screen/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/exercise2_redo_book_ui/widgets/custom_cart.dart';
+
+class HomeScreenRE extends StatelessWidget {
+  const HomeScreenRE({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "New Books",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(),
+            ),
+            Row(
+              children: const [
+                Custom_Card(
+                  imageURL:
+                      "https://i.natgeofe.com/n/3861de2a-04e6-45fd-aec8-02e7809f9d4e/02-cat-training-NationalGeographic_1484324_3x4.jpg",
+                ),
+              ],
+            ),
+            Text(
+              "For You",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(),
+            ),
+            Row(
+              children: const [],
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
