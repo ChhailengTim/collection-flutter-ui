f745ef9e8f6479126fd9ee81a2373516a4faded7
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-05 23:44:32 +0700

    Update main.dart

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/exercise1_book_ui/screen/screen.dart';
+
+import 'exercise2_redo_book_ui/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: HomeScreen(),
+        body: HomeScreenRE(),
       ),
     );
   }
