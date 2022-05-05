997e60be93d0c9ec6c753411b6285f67c76a0938
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-06 00:32:35 +0700

    Create screen_detail.dart

---
+++ flutter_ui/lib/exercise2_redo_book_ui/screen/screen_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class detail_screen extends StatelessWidget {
+  const detail_screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Detail Screen"),
+      ),
+    );
+  }
+}
