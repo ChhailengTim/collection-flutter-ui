6d28641d070f4567e85143222e94fe41d92ad6d9
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-06 00:32:33 +0700

    Update custom_cart.dart

--- flutter_ui/lib/exercise2_redo_book_ui/widgets/custom_cart.dart
+++ flutter_ui/lib/exercise2_redo_book_ui/widgets/custom_cart.dart
@@ -1,32 +1,58 @@
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class Custom_Card extends StatelessWidget {
   final String? imageURL;
   final String? title;
   final String? price;
-  const Custom_Card({Key? key, this.imageURL, this.price, this.title})
+  final GestureTapCallback? onTap;
+  const Custom_Card(
+      {Key? key, this.imageURL, this.price, this.title, this.onTap})
       : super(key: key);
 
-  Widget priceLable(String price) {
-    return Container();
+  Widget priceLable(BuildContext context, String price) {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        color: Colors.blue[100],
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Text(
+        price,
+        style: Theme.of(context).textTheme.titleSmall!.copyWith(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 1),
-      height: MediaQuery.of(context).size.height * 0.2,
-      width: MediaQuery.of(context).size.width * 0.4,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: NetworkImage(imageURL ?? ""),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            margin: const EdgeInsets.only(right: 5),
+            height: MediaQuery.of(context).size.height * 0.2,
+            width: MediaQuery.of(context).size.width * 0.4,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(imageURL ?? ""),
+              ),
+            ),
+          ),
         ),
-      ),
-    );
-    const SizedBox(
-      height: 5,
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          title ?? "No Title",
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(),
+        ),
+        priceLable(context, price ?? "Free"),
+      ],
     );
   }
 }
