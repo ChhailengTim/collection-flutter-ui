7773778ca86cd0819f5cf3e13c05eab8e7ad8006
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-07 23:18:53 +0700

    Create lesson2_button.dart

---
+++ flutter_ui/lib/1_materialapp_scaffold/lesson2_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ShowMe extends StatefulWidget {
+  const ShowMe({Key? key}) : super(key: key);
+
+  @override
+  State<ShowMe> createState() => _ShowMeState();
+}
+
+class _ShowMeState extends State<ShowMe> {
+  bool myNewButton = false;
+  String myText = "Hello";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Flutter example"),
+        ),
+        body: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  myNewButton = !myNewButton;
+                });
+              },
+              child: const Text("My tap"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(myNewButton ? "This is true" : myText),
+              ],
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          child: IconButton(
+            icon: const Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              debugPrint("Done");
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
