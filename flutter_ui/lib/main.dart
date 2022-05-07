777d04c48532681f525d3755d26dd0148d5e2a7a
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-07 23:00:33 +0700

    Update main.dart

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/1_materialapp_scaffold/lesson1_ex_redo.dart';
+import 'package:flutter_ui/1_materialapp_scaffold/lesson1_redo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Show(),
+        body: HomeMe(),
       ),
     );
   }
