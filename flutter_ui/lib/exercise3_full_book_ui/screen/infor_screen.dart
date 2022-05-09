ad6faf4223db6b8d190364f3e16da0480c91b157
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-09 23:53:35 +0700

    Create infor_screen.dart

---
+++ flutter_ui/lib/exercise3_full_book_ui/screen/infor_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InforScreen extends StatelessWidget {
+  const InforScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Detail Page"),
+      ),
+    );
+  }
+}
