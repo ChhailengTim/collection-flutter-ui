05cdaed77074145dde1db8074d3ece7e21297cfe
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-10 00:32:43 +0700

    Update infor_screen.dart

--- flutter_ui/lib/exercise3_full_book_ui/screen/infor_screen.dart
+++ flutter_ui/lib/exercise3_full_book_ui/screen/infor_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
 class InforScreen extends StatelessWidget {
-  final String? imageURL;
-  const InforScreen({Key? key, this.imageURL}) : super(key: key);
+  final String? imageURL, writer, price, about;
+  const InforScreen(
+      {Key? key, this.imageURL, this.writer, this.price, this.about})
+      : super(key: key);
+
+  Widget customBuyButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 70),
+        child: Text(
+          'Buy Now',
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              fontWeight: FontWeight.w500, fontSize: 16, color: Colors.white),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +51,12 @@ class InforScreen extends StatelessWidget {
               height: _heigh / 2,
             ),
           ),
+
+          ///
           Positioned(
             bottom: 0,
             child: Stack(
+              alignment: Alignment.bottomRight,
               children: [
                 Container(
                   decoration: const BoxDecoration(
@@ -41,28 +68,116 @@ class InforScreen extends StatelessWidget {
                   ),
                   height: (_heigh / 2) + 20,
                   width: _width,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+                  // alignment: Alignment.center,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 35),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        Row(
+                          children: [
+                            Text(
+                              'The Memories Police',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(fontWeight: FontWeight.bold),
+                            ),
+                            const Spacer(),
+                            Icon(
+                              Icons.bookmark,
+                              color: Colors.amber[700],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 5),
                         Text(
-                          "The Memories Police",
+                          'by ${writer ?? 'unknown'}',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        // const SizedBox(height: 1),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 16,
+                              color: Colors.amber[700],
+                            ),
+                            const SizedBox(width: 5),
+                            Icon(
+                              Icons.star,
+                              size: 16,
+                              color: Colors.amber[700],
+                            ),
+                            const SizedBox(width: 5),
+                            Icon(
+                              Icons.star,
+                              size: 16,
+                              color: Colors.amber[700],
+                            ),
+                            const SizedBox(width: 5),
+                            Icon(
+                              Icons.star,
+                              size: 16,
+                              color: Colors.amber[700],
+                            ),
+                            const SizedBox(width: 5),
+                            Icon(
+                              Icons.star,
+                              size: 16,
+                              color: Colors.amber[700],
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              '5.0',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                      color: Colors.amber[700], fontSize: 14),
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              '(1534)',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                      color: Colors.amber[700], fontSize: 14),
+                            ),
+                            const Spacer(),
+                            Text(
+                              price == null ? 'Unavialable' : '\$$price',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'About the book',
                           style: Theme.of(context)
                               .textTheme
-                              .titleLarge!
-                              .copyWith(fontWeight: FontWeight.bold),
+                              .titleMedium!
+                              .copyWith(fontWeight: FontWeight.w600),
                         ),
-                        const Spacer(),
-                        Icon(
-                          Icons.bookmark,
-                          color: Colors.amber[700],
+                        const SizedBox(height: 10),
+                        Text(
+                          about ?? '',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall!
+                              .copyWith(height: 1.5),
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
+                customBuyButton(context),
               ],
             ),
           ),
