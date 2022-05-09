9dd40f3791bd1cc7492d38689a5652e22fe9e0d1
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-10 00:22:50 +0700

    Update infor_screen.dart

--- flutter_ui/lib/exercise3_full_book_ui/screen/infor_screen.dart
+++ flutter_ui/lib/exercise3_full_book_ui/screen/infor_screen.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
 
 class InforScreen extends StatelessWidget {
-  const InforScreen({Key? key}) : super(key: key);
+  final String? imageURL;
+  const InforScreen({Key? key, this.imageURL}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double _heigh = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Detail Page"),
       ),
-      body: Column(),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          SizedBox(
+            height: _heigh,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(imageURL!),
+                ),
+              ),
+              height: _heigh / 2,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(25),
+                      topLeft: Radius.circular(25),
+                    ),
+                  ),
+                  height: (_heigh / 2) + 20,
+                  width: _width,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
