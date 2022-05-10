54258c9e81b6afd6aead4ed1e54c2756a1a7865c
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-10 22:10:04 +0700

    Create lesson2_button_solve.dart

---
+++ flutter_ui/lib/1_material_app_scaffold/lesson2_button_solve.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ButtonShow extends StatelessWidget {
+  const ButtonShow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
