316ca853c2f3fff6e22c6c40a9dcc8a14f924fa7
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-10 22:09:52 +0700

    Create lesson1_ex.dart

---
+++ flutter_ui/lib/1_material_app_scaffold/lesson1_ex.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class View extends StatelessWidget {
+  const View({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Flutter Coding Exercise"),
+        ),
+        // body: Column(
+        //   children: const [
+        //     Text("My body"),
+        //     Text("My body"),
+        //     Text("My body"),
+        //     Text("My body"),
+        //   ],
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("My body"),
+            const Text("My body"),
+            const Text("My body"),
+            const Text("My body"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text("Row simple"),
+                Text("Row simple"),
+                Text("Row simple"),
+                Text("Row simple"),
+              ],
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          child: IconButton(
+            onPressed: () {
+              debugPrint('Hello fluter code');
+            },
+            icon: const Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
