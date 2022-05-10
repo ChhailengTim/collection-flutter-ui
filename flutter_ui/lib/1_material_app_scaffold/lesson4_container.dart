df774f05f4cdbbccb21a0d0dc81fe14f8c48a47e
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-10 23:51:46 +0700

    Update lesson4_container.dart

--- flutter_ui/lib/1_material_app_scaffold/lesson4_container.dart
+++ flutter_ui/lib/1_material_app_scaffold/lesson4_container.dart
@@ -13,62 +13,68 @@ class Window extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              alignment: Alignment.center,
-              height: 50,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Text(
-                "123",
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(fontWeight: FontWeight.bold),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                // height: 50,
+                // width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Text(
+                  "123",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-            Row(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(5),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    // height: 50,
+                    width: MediaQuery.of(context).size.width / 3.5,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      "123",
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  child: Text(
-                    "123",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(fontWeight: FontWeight.bold),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      // height: 50,
+                      // width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        "123",
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge!
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Text(
-                    "123",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
             Container(
               alignment: Alignment.center,
-              height: 100,
-              width: 200,
+              height: MediaQuery.of(context).size.height / 2,
+              // width: 200,
               decoration: BoxDecoration(
                 color: Colors.purple,
                 borderRadius: BorderRadius.circular(5),
@@ -81,20 +87,22 @@ class Window extends StatelessWidget {
                     .copyWith(fontWeight: FontWeight.bold),
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              height: 50,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Text(
-                "123",
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(fontWeight: FontWeight.bold),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                // height: 50,
+                // width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Text(
+                  "123",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
               ),
             ),
           ],
