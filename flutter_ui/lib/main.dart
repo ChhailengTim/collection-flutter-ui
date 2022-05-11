7441d23774d2e6ceb60c1bf1dc8aa6a8950447e1
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-12 00:12:17 +0700

    Update main.dart

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/1_material_app_scaffold/lesson5_condition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Scaffold(),
+        body: Condition(),
       ),
     );
   }
