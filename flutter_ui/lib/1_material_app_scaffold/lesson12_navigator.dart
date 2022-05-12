efc6e4fb989aa8064b8a9a61b4508e4ec4845aef
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-12 22:29:42 +0700

    Create lesson12_navigator.dart

---
+++ flutter_ui/lib/1_material_app_scaffold/lesson12_navigator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/1_material_app_scaffold/lesson13_navigtor_new_page.dart';
+
+class TestNavigator extends StatelessWidget {
+  const TestNavigator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const NewPage()));
+          debugPrint("Tap");
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              Icons.arrow_forward,
+              size: 30,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.blue,
+        child: Icon(
+          Icons.home,
+          size: 50,
+        ),
+      ),
+    );
+  }
+}
