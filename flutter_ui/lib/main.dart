d21b611a8789917651df9245a25f635a120515d0
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-13 23:53:06 +0700

    Update main.dart

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/1_material_app_scaffold/lesson12_navigator.dart';
+import 'package:flutter_ui/1_material_app_scaffold/lesson17_listview_list_tile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: TestNavigator(),
+        body: MainList(),
       ),
     );
   }
