01cce0c4667cec43303aeadcd502d859b3ab381a
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-15 23:25:34 +0700

    Update front_screen.dart

--- flutter_ui/lib/2_login_screen/screen/front_screen.dart
+++ flutter_ui/lib/2_login_screen/screen/front_screen.dart
@@ -18,74 +18,79 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
               children: [
-                const Icon(
-                  Icons.coffee,
-                  color: Colors.white,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.coffee,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "WELCOME",
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ],
                 ),
-                Text(
-                  "WELCOME",
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      fontWeight: FontWeight.bold, color: Colors.white),
+                const Center(
+                  child: Image(
+                    image: NetworkImage(
+                        "https://github.com/fluttermapp/flutter_course_app_2/blob/main/images/welcome.png?raw=true"),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Sign in");
+                  },
+                  child: Container(
+                    child: Text(
+                      "Sign in",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(3.0),
+                        bottomRight: Radius.circular(3.0),
+                        bottomLeft: Radius.circular(3.0),
+                        topLeft: Radius.circular(3.0),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    debugPrint("Register");
+                  },
+                  child: Container(
+                    child: Text(
+                      "Register",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(3.0),
+                        bottomRight: Radius.circular(3.0),
+                        bottomLeft: Radius.circular(3.0),
+                        topLeft: Radius.circular(3.0),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
             ),
-            const Center(
-              child: Image(
-                image: NetworkImage(
-                    "https://github.com/fluttermapp/flutter_course_app_2/blob/main/images/welcome.png?raw=true"),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                debugPrint("Sign in");
-              },
-              child: Container(
-                child: Text(
-                  "Sign in",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(3.0),
-                    bottomRight: Radius.circular(3.0),
-                    bottomLeft: Radius.circular(3.0),
-                    topLeft: Radius.circular(3.0),
-                  ),
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            GestureDetector(
-              onTap: () {
-                debugPrint("Register");
-              },
-              child: Container(
-                child: Text(
-                  "Register",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(3.0),
-                    bottomRight: Radius.circular(3.0),
-                    bottomLeft: Radius.circular(3.0),
-                    topLeft: Radius.circular(3.0),
-                  ),
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
