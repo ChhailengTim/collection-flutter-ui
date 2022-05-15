a82cc352cd93cbc7093a61356c93181994cbcca5
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-15 21:56:26 +0700

    Update main.dart

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/1_material_app_scaffold/lesson22_text_filed_exercise.dart';
+import 'package:flutter_ui/2_login_screen/screen/fron_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: TextExerciseField(),
+        body: HomeScreen(),
       ),
     );
   }
