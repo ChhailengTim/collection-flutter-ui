5addfc42dad983a6535f8fd24ad8644eece851e8
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-15 23:21:27 +0700

    Update front_screen.dart

--- flutter_ui/lib/2_login_screen/screen/front_screen.dart
+++ flutter_ui/lib/2_login_screen/screen/front_screen.dart
@@ -6,8 +6,87 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent,
       appBar: AppBar(
-        title: const Text("WELCOME"),
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+          "Login UI",
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.coffee,
+                  color: Colors.white,
+                ),
+                Text(
+                  "WELCOME",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ],
+            ),
+            const Center(
+              child: Image(
+                image: NetworkImage(
+                    "https://github.com/fluttermapp/flutter_course_app_2/blob/main/images/welcome.png?raw=true"),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint("Sign in");
+              },
+              child: Container(
+                child: Text(
+                  "Sign in",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(3.0),
+                    bottomRight: Radius.circular(3.0),
+                    bottomLeft: Radius.circular(3.0),
+                    topLeft: Radius.circular(3.0),
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint("Register");
+              },
+              child: Container(
+                child: Text(
+                  "Register",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(3.0),
+                    bottomRight: Radius.circular(3.0),
+                    bottomLeft: Radius.circular(3.0),
+                    topLeft: Radius.circular(3.0),
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
