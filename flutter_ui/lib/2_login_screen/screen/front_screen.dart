c06991022003af07e634174fcafa52722bdcdd1c
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-19 23:57:58 +0700

    fix button

--- flutter_ui/lib/2_login_screen/screen/front_screen.dart
+++ flutter_ui/lib/2_login_screen/screen/front_screen.dart
@@ -18,79 +18,70 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.coffee,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "WELCOME",
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                  ],
+                const Icon(
+                  Icons.coffee,
+                  color: Colors.white,
                 ),
-                const Center(
-                  child: Image(
-                    image: NetworkImage(
-                        "https://github.com/fluttermapp/flutter_course_app_2/blob/main/images/welcome.png?raw=true"),
-                  ),
-                ),
-                Container(
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 15,
-                        ),
-                      ),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                      ),
-                    ),
-                    child: const Text("Sign in"),
-                    onPressed: () {
-                      debugPrint("Sign in");
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 15,
-                      ),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                  child: const Text("Register"),
-                  onPressed: () {
-                    debugPrint("Register");
-                  },
+                Text(
+                  "WELCOME",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.white),
                 ),
               ],
             ),
-          ),
+            Container(
+              child: const Image(
+                image: NetworkImage(
+                    "https://github.com/fluttermapp/flutter_course_app_2/blob/main/images/welcome.png?raw=true"),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 30,
+              margin: const EdgeInsets.symmetric(horizontal: 100),
+              alignment: Alignment.center,
+              child: const Text(
+                "Sign In",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.white),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3),
+                //color: Colors.white,
+                border: Border.all(color: Colors.white, width: 3),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              width: double.infinity,
+              height: 30,
+              margin: const EdgeInsets.symmetric(horizontal: 100),
+              alignment: Alignment.center,
+              child: const Text(
+                "Register",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.deepOrange),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(3),
+              ),
+            ),
+          ],
         ),
       ),
     );
