269deaf262408615ecb2cb4d24fde10aa3724dd5
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-21 00:49:39 +0700

    Update screen.dart

--- traval_ui/travel_ui/lib/screen.dart
+++ traval_ui/travel_ui/lib/screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class HomePageTravel extends StatelessWidget {
+  const HomePageTravel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset("images/icon.png"),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+                "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/09/travel-logo-design.jpg"),
+            Text(
+              "Travel App",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 80,
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text("Stays"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
