17dae9ddc5b52c7e1a5c55b32d84ad144a51bfd8
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-05-21 16:39:06 +0700

    Create login_screen.dart

---
+++ flutter_ui/lib/2_login_screen/screen/login_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Icon(
+                Icons.house,
+                size: 100,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Join or sign in to unlock the full experience",
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontSize: 15,
+                    ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Continue with Email",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.white),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(3),
+                  //border: Border.all(color: Colors.black, width: 3),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Continue with Google",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.black),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Continue with Facebook",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.black),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "By joining you agree to our Terms and Privacy Policy",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                    color: Colors.black26),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
