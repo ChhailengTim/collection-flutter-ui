0fa111bad33e123b283f910faf025d3f0ff05376
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-22 00:04:58 +0700

    Update signup_screen.dart

--- flutter_ui/lib/2_login_screen/screen/signup_screen.dart
+++ flutter_ui/lib/2_login_screen/screen/signup_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
