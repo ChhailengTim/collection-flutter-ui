a78cb25074d5e971aa4fd22ad8dcb837547ff236
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-22 17:00:34 +0700

    fix

---
+++ flutter_ui/lib/real-estate-app/screen/login_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.house,
+                  size: 100,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "Join or sign in to unlock the full experience",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontSize: 15,
+                      ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Continue with Email",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(3),
+                    //border: Border.all(color: Colors.black, width: 3),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  alignment: Alignment.center,
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "image/google_logo.png",
+                          width: 20,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          "Continue with Google",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  alignment: Alignment.center,
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "image/facebook_logo.png",
+                          width: 20,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          "Continue with Facebook",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(color: Colors.black, width: 2),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text(
+                  "By joining you agree to our Terms and Privacy Policy",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                      color: Colors.black26),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
