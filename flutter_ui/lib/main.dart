3d00b7e43c0fc581b3594ef20f20f71dacf46c71
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-27 00:18:27 +0700

    Update main.dart

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/real-estate-app/screen/home_scree.dart';
+
+import 'push_notification/screen/push_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: HomeScreen(),
+        body: PushScreen(),
       ),
     );
   }
