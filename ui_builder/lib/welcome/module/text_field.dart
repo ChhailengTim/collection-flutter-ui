007a9b6dbefbb76ab2d95ff198249683fbbf36ee
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-28 17:25:36 +0700

    Create text_field.dart

---
+++ ui_builder/lib/welcome/module/text_field.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  const TextFieldContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container();
+  }
+}
