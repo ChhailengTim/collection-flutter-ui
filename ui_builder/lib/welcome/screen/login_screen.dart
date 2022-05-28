53cde7c92c641e7ea00526184ff332f8192a8f95
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-28 17:25:38 +0700

    Create login_screen.dart

---
+++ ui_builder/lib/welcome/screen/login_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:ui_builder/welcome/module/button.dart';
+import 'package:ui_builder/welcome/module/email_phone_input.dart';
+import 'package:ui_builder/welcome/module/password_input.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              const Icon(
+                Icons.logo_dev,
+                size: 150,
+              ),
+              const EmailPhoneInput(
+                text: "Email or Phone",
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const PasswordInput(
+                text: "Password",
+              ),
+              Button(
+                text: "Log in",
+                press: () {},
+                color: Colors.blueAccent,
+                textColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
