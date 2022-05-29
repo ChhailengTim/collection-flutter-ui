87cb29f4a8fe9e38fdf36e44394afa189080149b
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-29 22:59:51 +0700

    Create confirm_code_screen.dart

---
+++ ui_builder/lib/welcome/screen/confirm_code_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:ui_builder/welcome/module/button.dart';
+import 'package:ui_builder/welcome/module/confirm_code.dart';
+
+import 'create_new_password_screen.dart';
+
+class ConfirmCodeScreen extends StatelessWidget {
+  const ConfirmCodeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Confirm code"),
+      ),
+      body: Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Reset password",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                  "Enter the Email or Phone associated with your account and we'll send a code instruction to reset your password."),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const ConfirmCode(
+            text: "Enter code",
+          ),
+          Button(
+            press: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreateNewPassword()));
+            },
+            text: "Confirm code",
+            textColor: Colors.white,
+            color: Colors.purple,
+          ),
+        ],
+      ),
+    );
+  }
+}
