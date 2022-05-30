a33e418cd4fc4d6b2c00c6e420e7c550b51fb0e7
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-05-30 11:00:55 +0700

    Update login_screen.dart

--- ui_builder/lib/welcome/screen/login_screen.dart
+++ ui_builder/lib/welcome/screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:ui_builder/welcome/module/button.dart';
 import 'package:ui_builder/welcome/module/email_phone_input.dart';
 import 'package:ui_builder/welcome/module/password_input.dart';
@@ -17,82 +18,105 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              const Icon(
-                Icons.logo_dev,
-                size: 150,
-              ),
-              const EmailPhoneInput(
-                text: "Email or Phone",
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const PasswordInput(
-                text: "Password",
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ResetPassword()));
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 215),
-                  child: RichText(
-                    textScaleFactor: 1,
-                    text: const TextSpan(
-                      text: "Forgot your password?",
-                      style: TextStyle(
-                        color: Colors.blueAccent,
-                        fontSize: 15,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.logo_dev,
+                  size: 150,
+                ),
+                const EmailPhoneInput(
+                  text: "Email or Phone",
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const PasswordInput(
+                  text: "Password",
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ResetPassword()));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 215),
+                    child: RichText(
+                      textScaleFactor: 1,
+                      text: const TextSpan(
+                        text: "Forgot your password?",
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontSize: 15,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Button(
-                text: "Log in",
-                press: () {},
-                color: Colors.blueAccent,
-                textColor: Colors.white,
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  RichText(
-                    textScaleFactor: 1,
-                    text: const TextSpan(
-                      text: "Don't have an account?",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
+                Button(
+                  text: "Log in",
+                  press: () {},
+                  color: Colors.blueAccent,
+                  textColor: Colors.white,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("-"),
+                    Text("OR"),
+                    Text("-"),
+                  ],
+                ),
+                SignInButton(
+                  Buttons.Facebook,
+                  onPressed: () {},
+                ),
+                SignInButton(
+                  Buttons.Google,
+                  onPressed: () {},
+                ),
+                SignInButton(
+                  Buttons.Twitter,
+                  onPressed: () {},
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      textScaleFactor: 1,
+                      text: const TextSpan(
+                        text: "Don't have an account?",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const SignUpScreen()));
-                    },
-                    child: const Text(
-                      "Create",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                    const SizedBox(
+                      width: 5,
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpScreen()));
+                      },
+                      child: const Text(
+                        "Create",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
