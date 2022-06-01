9d07f4f778aef8cc6f27a513841b529ed28e219f
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-02 00:46:05 +0700

    controller on change variable

---
+++ ui_builder/lib/change_colors/controller/change.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class Change extends GetxController {
+  var color = true.obs;
+  var textcolor = false.obs;
+  var iconcolor = false.obs;
+  var laptopmaccolor = false.obs;
+}
