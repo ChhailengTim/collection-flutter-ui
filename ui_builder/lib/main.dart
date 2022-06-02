ea758181817849f3b5fcf7e935c235868a547cdf
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-03 00:05:33 +0700

    Update main.dart

--- ui_builder/lib/main.dart
+++ ui_builder/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ui_builder/change_colors/change_colors.dart';
+
+import 'slider/slider_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChangeColors(),
+      home: SliderScreen(),
     );
   }
 }
