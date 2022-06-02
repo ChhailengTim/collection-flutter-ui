0113e92a97d777d591bf8e457b50e7c1c252c8fa
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-03 00:05:31 +0700

    Create slider_screen.dart

---
+++ ui_builder/lib/slider/slider_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SliderScreen extends StatefulWidget {
+  const SliderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SliderScreen> createState() => _SliderScreenState();
+}
+
+class _SliderScreenState extends State<SliderScreen> {
+  double range = 1.0;
+  double start = -1;
+  double stop = -1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Slider with setState"),
+      ),
+      body: Column(
+        children: [
+          Text(
+            range.round().toString(),
+          ),
+          Slider(
+            value: range,
+            min: 0,
+            max: 100,
+            divisions: 100,
+            onChanged: (double v) {
+              setState(() {
+                range = v;
+              });
+            },
+            onChangeStart: (double v) {
+              setState(() {
+                start = v;
+              });
+            },
+            onChangeEnd: (double v) {
+              setState(() {
+                stop = v;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
