632a8f022b68c071eba23e1e8a0f1628b7fe218e
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-04 00:43:12 +0700

    Update main.dart

--- ui_builder/lib/main.dart
+++ ui_builder/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ui_builder/future_builder/futurebuilder.dart';
+
+import 'stream_builder/stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FutureBuilderScreen(),
+      home: StreamScreen(),
     );
   }
 }
