4fb458fc5326cacdb2bd741d1b2c02fec87c5fc0
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-04 00:17:10 +0700

    futurebuilder

--- ui_builder/lib/future_builder/future.dart
+++ ui_builder/lib/future_builder/future.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class FutureData extends StatefulWidget {
   const FutureData({Key? key}) : super(key: key);
@@ -22,6 +21,36 @@ class _FutureDataState extends State<FutureData> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Future Data"),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Text(
+                snapshot.error.toString(),
+              );
+            } else {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    snapshot.data.toString(),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    child: const Text("Refresh"),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
