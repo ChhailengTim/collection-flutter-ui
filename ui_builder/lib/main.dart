2ed22a9f922cdf81d9cdc6bbce15c94180edf5a8
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-06 11:54:42 +0700

    Update main.dart

--- ui_builder/lib/main.dart
+++ ui_builder/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ui_builder/api_start/screen/home_scree.dart';
+
+import 'post_api/screen/post_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: PostScren(),
     );
   }
 }
