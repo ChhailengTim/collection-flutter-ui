59a7a0b8498162edf240eea04c9502b0ff68c0d8
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-06 11:54:41 +0700

    Create https.dart

---
+++ ui_builder/lib/post_api/controller/https.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'package:ui_builder/post_api/module/posts.dart';
+
+class HttpRequest {
+  Future<List<Posts>?> getPosts() async {
+    var client = http.Client();
+    var uri = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postsFromJson(json);
+    }
+    return null;
+  }
+}
