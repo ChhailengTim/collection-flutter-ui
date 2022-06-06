feee5dd11431cc48798c91e0cdd77cc4ee2fa792
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-06 10:01:54 +0700

    fetch data API

--- ui_builder/lib/api_start/controller/https.dart
+++ ui_builder/lib/api_start/controller/https.dart
@@ -2,11 +2,14 @@ import 'package:ui_builder/api_start/module/post.dart';
 import 'package:http/http.dart' as http;
 
 class HttpsScreen {
-  Future<List<Post>?> getPost() async {
+  Future<List<Post>?> getPosts() async {
     var client = http.Client();
-
     var uri = Uri.parse("https://jsonplaceholder.typicode.com/posts");
-    var respond = await client.get(uri);
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postFromJson(json);
+    }
     return null;
   }
 }
