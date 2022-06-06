761677516a1a047ea8323794c2934937aa338a9b
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-06 13:58:13 +0700

    fetch API Comment

--- ui_builder/lib/main.dart
+++ ui_builder/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'api_post/screen/post_screen.dart';
+import 'package:ui_builder/api_comment/screen/comment_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PostScren(),
+      home: CommentScreen(),
     );
   }
 }
