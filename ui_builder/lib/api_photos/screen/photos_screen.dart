89c7bfdf1c7da89dba5e7f2ca35b976bb7975b38
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-07 23:53:40 +0700

    fetch api

---
+++ ui_builder/lib/api_photos/screen/photos_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PhotosScreen extends StatefulWidget {
+  const PhotosScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PhotosScreen> createState() => _PhotosScreenState();
+}
+
+class _PhotosScreenState extends State<PhotosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Photos API"),
+      ),
+    );
+  }
+}
