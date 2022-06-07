ec24c6fe19952107b6b4d929fb01d6d224138190
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-08 00:43:42 +0700

    fetch data api

--- ui_builder/lib/api_photos/screen/photos_screen.dart
+++ ui_builder/lib/api_photos/screen/photos_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ui_builder/api_photos/controller/http_photos.dart';
+import 'package:ui_builder/api_photos/module/photos.dart';
 
 class PhotosScreen extends StatefulWidget {
   const PhotosScreen({Key? key}) : super(key: key);
@@ -8,11 +10,63 @@ class PhotosScreen extends StatefulWidget {
 }
 
 class _PhotosScreenState extends State<PhotosScreen> {
+  List<PhotosApi>? photosapi;
+  var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getPhotos();
+  }
+
+  getPhotos() async {
+    photosapi = await HttpPhotos().getPhotos();
+    if (photosapi != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Photos API"),
+      ),
+      body: Visibility(
+        visible: isLoaded,
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+        child: ListView.builder(
+          itemCount: photosapi?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Text(
+                    photosapi![index].albumId.toString(),
+                  ),
+                  Image(
+                    image: NetworkImage(photosapi![index].url!),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Image(
+                    image: NetworkImage(photosapi![index].thumbnailUrl!),
+                  ),
+                  Text(
+                    photosapi![index].id.toString(),
+                  ),
+                  Text(photosapi![index].title ?? "title"),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
