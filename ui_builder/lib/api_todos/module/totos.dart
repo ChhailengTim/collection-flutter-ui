540219c48a4b88a946e961cc6ed09c597285747d
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-08 11:43:34 +0700

    fetch api todos

---
+++ ui_builder/lib/api_todos/module/totos.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+List<Todos> todosFromJson(String str) =>
+    List<Todos>.from(json.decode(str).map((x) => Todos.fromJson(x)));
+
+String todosToJson(List<Todos> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Todos {
+  Todos({
+    this.userId,
+    this.id,
+    this.title,
+    this.completed,
+  });
+
+  int? userId;
+  int? id;
+  String? title;
+  bool? completed;
+
+  factory Todos.fromJson(Map<String, dynamic> json) => Todos(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        completed: json["completed"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "completed": completed,
+      };
+}
