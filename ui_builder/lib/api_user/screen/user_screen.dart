39121db2c9bdd4dfdbe9b1aee6b21eeba74d5368
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-08 12:00:53 +0700

    Create user_screen.dart

---
+++ ui_builder/lib/api_user/screen/user_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatefulWidget {
+  const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User API"),
+      ),
+    );
+  }
+}
