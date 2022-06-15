8c003951bb6a4de3bdf81e381a936b4e225b677f
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-16 00:13:37 +0700

    Update main.dart

--- getx_api/lib/main.dart
+++ getx_api/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'post_api/screens/post_screen.dart';
-
+import 'photos_api/screen/photo_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PostScreen(),
+      home: PhotoScreen(),
     );
   }
 }
