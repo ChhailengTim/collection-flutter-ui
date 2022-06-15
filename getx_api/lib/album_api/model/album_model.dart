be599a03bd190ba44b5abb7853fece625a268600
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-16 00:13:24 +0700

    Create album_model.dart

---
+++ getx_api/lib/album_api/model/album_model.dart
@@ -0,0 +1,21 @@
+class AlbumModel {
+  int? userId;
+  int? id;
+  String? title;
+
+  AlbumModel({this.userId, this.id, this.title});
+
+  AlbumModel.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['userId'] = userId;
+    data['id'] = id;
+    data['title'] = title;
+    return data;
+  }
+}
