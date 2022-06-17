974f0e12894e9791858341480439a6f6a6ed5aa4
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-17 15:49:20 +0700

    Update main.dart

--- getx_api/lib/main.dart
+++ getx_api/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:getx_api/user_api/screen/user_screen.dart';
+
+import 'tik_profile/screen/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UserScreen(),
+      home: TikTokProfileScreen(),
     );
   }
 }
