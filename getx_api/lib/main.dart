2f19fe4d50c0bec2ff4101318496268480f58c01
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-21 09:01:34 +0700

    Update main.dart

--- getx_api/lib/main.dart
+++ getx_api/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:getx_api/deatail_screen/screen/custom_detail_screen.dart';
+import 'package:getx_api/deatail_screen/screen/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CustomDetailScreen(),
+      home: DetialScreen(),
     );
   }
 }
